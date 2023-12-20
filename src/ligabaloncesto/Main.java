6d771e96e3673909200b9d83eaaaee10e9860f10
angie duran <117625258+duranangie@users.noreply.github.com>
2023-12-20 13:28:07 -0500

    feat: :sparkles:  inicio de las segunda liga

--- src/ligabaloncesto/Main.java
+++ src/ligabaloncesto/Main.java
@@ -16,96 +16,178 @@ public class Main {
      * @param args the command line arguments
      */
    
-    public static void main(String[] args) {
+   public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int opcion = -1;
-        partidoLiga partidoliga = new partidoLiga(1, "Equipo A", "Equipo B", 0, 0, false, "2023-01-01");
-
+        int opciones = -1;
+        partidoLiga partidoliga = new partidoLiga(1, "equipo Local", "Equipo Visitante", 0, 0, false, "2023-01-01");
+        PartidoPlayOff partidoPlay = new PartidoPlayOff("octavos", "equipo Local", "Equipo Visitante", 0, 0, false, "2023-01-01");
+       
+        do{
+            try{
+                System.out.println("------------------------------------------");
+                System.out.println("BIENVENIDOS A SUS PARTIDOS FAVORITOS");
+                System.out.println("------------------------------------------");
+                System.out.println("¿Que partido deseas seleccionar?");
+                System.out.println("1. Partido play Off");
+                System.out.println("2. Partido liga");
+                System.out.println("0. Salir");
+                System.out.println("Ingresa la opcion de tu preferencia (0-2)");
+                
+                if(opciones == 1){
+                    
+                }
+                else if(opciones ==2){
+                
+                
+            }catch(Exception e){
+                System.err.println("Error");
+                opciones = -1;
+                continue;
+            }
+            
+            
+        }while(opciones !=0);
+        
+        
+        
         do {
-            System.out.println("Bienvenido al partido");
-            System.out.println("1. obtener el resultado");
-            System.out.println("2. registrar puntos para los locales");
-            System.out.println("3. registrar puntos para los visitantes");
-            System.out.println("4.obtener el ganador");
-            System.out.println("0.Salir");
+
+            try {
+                System.out.println("-------------------------------------------");
+                System.out.println("Bienvenido al partido");
+                System.out.println("-------------------------------------------");
+                System.out.println("1. obtener el resultado");
+                System.out.println("2. registrar puntos para los locales");
+                System.out.println("3. registrar puntos para los visitantes");
+                System.out.println("4.obtener el ganador");
+                System.out.println("0.Salir");
+                System.out.println("------------------------------------------");
 
                 if (scanner.hasNextLine()) {
-                String input = scanner.nextLine().trim(); //Leer espacios en blanco
-               
-                if (input.isEmpty()) {
-                    System.err.println("Verifica nuevamene!");
-                }
+                    String input = scanner.nextLine().trim(); //Leer espacios en blanco
 
-                if (input.matches("\\d+")) {
-                    opcion = Integer.parseInt(input);
+                    if (input.isEmpty()) {
+                        System.err.println("Verifica nuevamene!");
+                    }
+
+                    if (input.matches("\\d+")) {
+                        opcion = Integer.parseInt(input);
+                    } else {
+                        System.err.println("Error ingresa solo numeros enteros");
+                            continue; // Se reinicia el ciclo 
+                        }
+
+                    switch (opcion) {
+                        case 1:
+                            System.out.println("el partido quedo " + partidoliga.getCestasLocal() + "-" + partidoliga.getCestasVisitante());
+                            break;
+
+                        case 2:
+                            System.out.println("Ingresa los puntos para los locales ");
+
+                            if (scanner.hasNextLine()) {
+                                String inputs = scanner.nextLine().trim(); // Leer espacios en blanco
+
+                                if (input.isEmpty()) {
+                                    System.err.println("Error: Debes ingresar un valor.");
+                                    continue; // Reiniciar el bucle
+                                }
+
+                                if (inputs.matches("\\d+")) {
+                                    int puntoLocales = Integer.parseInt(inputs);
+
+                                    if (puntoLocales > 0) {
+                                        partidoliga.setCestasLocal(partidoliga.getCestasLocal() + puntoLocales);
+                                    } else {
+                                        System.out.println("Error en el sistema, verifica nuevamente");
+                                    }
+                                } else {
+                                    System.err.println("Error: Recuerda que solo puedes ingresar números.");
+                                    continue; // Reiniciar el bucle
+                                }
+                            } else {
+                                System.err.println("Error: Recuerda que solo puedes ingresar números.");
+                                continue; // Reiniciar el bucle
+                            }
+
+                            break;
+
+                        case 3:
+                            System.out.println("Ingresa punto para los visitantes: ");
+                            if (scanner.hasNextLine()) {
+                                String inp = scanner.nextLine().trim();
+
+                                if (input.isEmpty()) {
+                                    System.err.println("Error debes ingresar un valor");
+                                    continue;
+                                }
+                                if (inp.matches("\\d+")) {
+                                    int puntoVisitante = Integer.parseInt(inp);
+
+                                    if (puntoVisitante > 0) {
+                                        partidoliga.setCestasVisitante(partidoliga.getCestasVisitante() + puntoVisitante);
+
+                                    } else {
+                                        System.err.println("ERROR EN LOS NUMEROS INGRESADOS");
+                                    }
+                                } else {
+                                    System.err.println("Recuerda que solo podes ingresar numeros al sistema!");
+                                    continue;
+                                }
+                            } else {
+                                System.err.println("Error en los datos verificaNuevamente");
+
+                                continue;
+                            }
+                            break;
+
+                        case 4:
+
+                            int cestasLocal = partidoliga.getCestasLocal();
+                            int cestasVisitante = partidoliga.getCestasVisitante();
+
+                            String ganador;
+                            if (cestasLocal > cestasVisitante) {
+                                ganador = partidoliga.getEquipoLocal();
+                                System.out.println("El ganador del partido es " + ganador);
+                            } else if (cestasLocal < cestasVisitante) {
+                                ganador = partidoliga.getEquipoVisitante();
+                                System.out.println(ganador);
+                            } else {
+                                ganador = "Empate";
+                                System.out.println("¡El partido terminó en empate!");
+                            }
+
+                            break;
+
+                        case 0:
+                            System.out.println("Gracias por ingresar");
+                            break;
+                        default:
+                            System.out.println("ERROR EN EL SISTEMA VERFICA NUEVAMENTE!");
+                    }
                 } else {
-                    System.err.println("Error ingresa solo numeros enteros");
-                    continue; // Se reinicia el ciclo 
+                    System.err.println("ERROR recuerda que solo puedes ingresar numeros !");
+                    scanner.next();
+                    opcion = -1;
+
                 }
 
-                switch (opcion) {
-                    case 1:
-                        System.out.println("el partido quedo " + partidoliga.getCestasLocal() + "-" + partidoliga.getCestasVisitante());
-                        break;
-
-                    case 2:
-
-                        System.out.println("Ingresa los puntos para los locales ");
-                        scanner.nextLine();
-                        if (scanner.hasNextInt()) {
-                            int puntoLocales = scanner.nextInt();
-
-                            if (puntoLocales > 0) {
-                                partidoliga.setCestasLocal(partidoliga.getCestasLocal() + puntoLocales);
-                            } else {
-                                System.out.println("Error en el sistema verifica nuevamente");
-                            }
-
-                        } else {
-                            System.err.println("Error recuerda que solo puedes ingresar numeros");
-                            scanner.next(); //esto es pra los bucles infinitos para que vuelva otra vez
-                        }
-
-                        break;
-                    case 3:
-                        System.out.println("Ingresa punto para los visitantes: ");
-                        if (scanner.hasNextInt()) {
-                            int puntoVisitante = scanner.nextInt();
-                            if (puntoVisitante > 0) {
-                                partidoliga.setCestasVisitante(partidoliga.getCestasVisitante() + puntoVisitante);
-
-                            } else {
-                                System.err.println("ERROR EN LOS NUMEROS INGRESADOS");
-                            }
-                        } else {
-                            System.err.println("Recuerda que solo podes ingresar numeros al sistema!");
-                            scanner.next();
-                        }
-                        break;
-
-                    case 4:
-                        System.out.println("El ganador del partido es: ");
-                        String ganador = (partidoliga.getCestasLocal() > partidoliga.getCestasVisitante()) ? partidoliga.getEquipoLocal() : partidoliga.getEquipoVisitante();
-                        System.out.println("El ganador de partido es " + ganador);
-                        break;
-
-                    case 0:
-                        System.out.println("Gracias por ingresar");
-                        break;
-                    default:
-                        System.out.println("ERROR EN EL SISTEMA VERFICA NUEVAMENTE!");
-                }
-            } else {
-                System.err.println("ERROR recuerda que solo puedes ingresar numeros !");
-                scanner.next();
+            } catch (Exception e) {
+                System.err.println("ERROR: Ocurrió un error inesperado, ingresa un enter para volver al menu: " );
                 opcion = -1;
+                continue;
             }
+       
 
-        } while (opcion != 0);
+        
+    
+        }while (opcion!= 0);
 
-        scanner.close();
+        scanner.close ();
 
-    }
+}
 
 }
 
