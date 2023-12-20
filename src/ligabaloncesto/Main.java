f8cc833757968e7bf3a9900e129aa4a167a78895
angie duran <117625258+duranangie@users.noreply.github.com>
2023-12-19 23:07:29 -0500

    feat: ✨ Validaciones
    Se realizaron las validaciones

--- src/ligabaloncesto/Main.java
+++ src/ligabaloncesto/Main.java
@@ -1,23 +1,24 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-
-package com.mycompany.ligabaloncesto;
+package ligabaloncesto;
 
 import java.util.Scanner;
 
 /**
  *
- * @author user
+ * @author ANGIE DURAN
  */
 public class Main {
 
+    /**
+     * @param args the command line arguments
+     */
     public static void main(String[] args) {
-       Scanner scanner = new Scanner(System.in);
-        int opcion; 
-        
-        
-        
+        Scanner scanner = new Scanner(System.in);
+        int opcion;
+
         partidoLiga partidoliga = new partidoLiga(1, "Equipo A", "Equipo B", 5, 0, false, "2023-01-01");
 
         do {
@@ -27,39 +28,69 @@ public class Main {
             System.out.println("3. registrar puntos para los visitantes");
             System.out.println("4.obtener el ganador");
             System.out.println("0.Salir");
+            if (scanner.hasNextInt()) {
+                opcion = scanner.nextInt();
 
-            opcion = scanner.nextInt();
+                switch (opcion) {
+                    case 1:
+                        System.out.println("el partido quedo " + partidoliga.getCestasLocal() + "-" + partidoliga.getCestasVisitante());
+                        break;
 
-            switch (opcion) {
-                case 1:
-                        System.out.println("el partido quedo " +partidoliga.getCestasLocal()+ "-" + partidoliga.getCestasVisitante());
-                    break;
+                    case 2:
 
-                case 2:
-                    System.out.println("Ingresa los puntos para los locales ");
-                    int puntosLocales = scanner.nextInt();
-                    partidoliga.setCestasLocal(partidoliga.getCestasLocal() + puntosLocales);
-                    break;
-                case 3:
-                    System.out.println("Ingresa punto para los visitantes: ");
-                    int puntoVisitante = scanner.nextInt();
-                    partidoliga.setCestasVisitante(partidoliga.getCestasVisitante() + puntoVisitante);
-                    break;
+                        System.out.println("Ingresa los puntos para los locales ");
+                        if (scanner.hasNextInt()) {
+                            int puntoLocales = scanner.nextInt();
 
-                case 4:
-                    System.out.println("El ganador del partido es: ");
-                    String ganador = (partidoliga.getCestasLocal() > partidoliga.getCestasVisitante()) ? partidoliga.getEquipoLocal() : partidoliga.getEquipoVisitante();
-                    System.out.println("El ganador de partido es "+ ganador);
-                    break;
+                            if (puntoLocales > 0) {
+                                partidoliga.setCestasLocal(partidoliga.getCestasLocal() + puntoLocales);
+                            } else {
+                                System.out.println("Error en el sistema verifica nuevamente");
+                            }
 
-                case 0:
-                    System.out.println("Gracias por ingresar");
-                    break;
-                default:
-                    System.out.println("ERROR EN EL SISTEMA VERFICA NUEVAMENTE!");
+                        } else {
+                            System.err.println("Error recuerda que solo puedes ingresar numeros");
+                            scanner.next(); //esto es pra los bucles infinitos para que vuelva otra vez
+                        }
+
+                        break;
+                    case 3:
+                        System.out.println("Ingresa punto para los visitantes: ");
+                        if (scanner.hasNextInt()) {
+                            int puntoVisitante = scanner.nextInt();
+                            if (puntoVisitante > 0) {
+                                partidoliga.setCestasVisitante(partidoliga.getCestasVisitante() + puntoVisitante);
+
+                            } else {
+                                System.err.println("ERROR EN LOS NUMEROS INGRESADOS");
+                            }
+                        } else {
+                            System.err.println("Recuerda que solo podes ingresar numeros al sistema!");
+                            scanner.next();
+                        }
+                        break;
+
+                    case 4:
+                        System.out.println("El ganador del partido es: ");
+                        String ganador = (partidoliga.getCestasLocal() > partidoliga.getCestasVisitante()) ? partidoliga.getEquipoLocal() : partidoliga.getEquipoVisitante();
+                        System.out.println("El ganador de partido es " + ganador);
+                        break;
+
+                    case 0:
+                        System.out.println("Gracias por ingresar");
+                        break;
+                    default:
+                        System.out.println("ERROR EN EL SISTEMA VERFICA NUEVAMENTE!");
+                }
+            } else {
+                System.err.println("ERROR recuerda que solo puedes ingresar numeros !");
+                scanner.next();
+                opcion = -1;
             }
 
         } while (opcion != 0);
+
+        scanner.close();
 
     }
 
